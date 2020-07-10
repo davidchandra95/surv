c685aa8f16c8e2e5584c58ce052bb49a99430e9a
davidchandra95 <david.chandra@tokopedia.com>
2020-07-11 01:03:36 +0700

    fix create react app proxy and add payment componennt

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import Payments from './Payments';
 
 class Header extends Component {
   renderContent() {
@@ -13,9 +14,10 @@ class Header extends Component {
           <li><a href="/auth/google">Login with Google</a></li>
         )
       default:
-        return (
+        return [
+          <li><Payments /></li>,
           <li><a href="/api/logout">Logout</a></li>
-        )
+        ]
     }
   }
 
@@ -27,7 +29,7 @@ class Header extends Component {
           <Link
             to={this.props.auth ? '/surveys' : '/'}
             className="left brand-logo"
-            style={{ paddingLeft: '10px'}}
+            style={{ paddingLeft: '10px' }}
           >
             Surv
           </Link>
