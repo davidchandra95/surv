c4b6e02e5455bc32c93f140a30fe46a58b5aaad1
davidchandra95 <david.chandra@tokopedia.com>
2020-07-11 00:00:46 +0700

    add cred

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+    googleClientID: '484149717281-b2crb61no3p33v42lepksqe58sunvdgv.apps.googleusercontent.com',
+    googleClientSecret: 'j1QRBUsBMxYgG1i_6LKPdISG',
+    mongoURI: 'mongodb+srv://admin123:admin@petdoption-vvv85.mongodb.net/surv?retryWrites=true&w=majority',
+    cookieKey: 'secret',
+    stripePublishableKey: 'pk_test_51H2gC6IPezWqhaNSLO0GKav6vCgfbfUVwag2C6K6NZAKymS76b7SfVv9UfydvXaL6r7t9BxmsANqSzFqgeW8qfAj00CF3WVCno',
+    stripeSecretKey: 'sk_test_51H2gC6IPezWqhaNSEo56MFgSIcw0iZwDeFckav5DtKmHpiVdiUMnxem8MhTRwQkKZh868q6nmEhG5Q8h91oQLmxQ00Ood90uiF'
+};
